0059d2a0626a2b795b0bf0d1ca69662f8b8734e1
Aram Hekimian <guerrerohek121@gmail.com>
2025-01-23 11:10:38 -0600

    Finished main register page design

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/log-in", (req, res) => res.render("login"));
+app.use("/register", (req, res) => res.render("register"));
 app.use("/", (req, res) => res.render("home"));
 
 app.listen(3000, () => console.log("Server up at port 3000"));
